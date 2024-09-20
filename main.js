87e3a707288d50d89d0825018e9fe3fd06565419
snagarajan30 <nagarajan3032002@gmail.com>
2024-09-20 09:40:30 +0530

    doctors page changes

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
 
-
-// // function nextSlide() {
-// //     const nextIndex = (currentIndex + 1) % slides.length;
-// //     showSlide(nextIndex);
-// // }
-
-// // function prevSlide() {
-// //     const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
-// //     showSlide(prevIndex);
-// // }
-
-// // lines.forEach((line, index) => {
-// //     line.addEventListener('click', () => {
-// //         if (index !== currentIndex) {
-// //             showSlide(index);
-// //         }
-// //     });
-// // });
-
 // Initial display
 showSlide(currentIndex);
 
@@ -31,38 +12,6 @@ hamburger.addEventListener('click', () => {
 
 
 
-// $(document).ready(function() {if ( $(window).width() < 835 ) {startCarouselyt();} else {$('.profileCarousel').addClass('off');}});    
-//          $(window).resize(function() {if ( $(window).width() < 835 ) {startCarouselyt();} else {stopCarousel();}});
-// 		 function startCarouselyt(){       
-//            $(".profileCarousel").owlCarousel({
-//               slideSpeed : 500,
-//               margin:10,
-//               paginationSpeed : 400,
-//               autoplay:true,
-//                autoplayHoverPause:true,
-//               items : 1,
-//                 dots: true,
-//               loop:true,
-//               nav:true,
-//               responsive: {
-//     0: {
-//         items: 1
-//     },
-//     769: { // You can use 600 or 767 as per your preference
-//         items: 2
-//     },
-//     835: {
-//         items: 3
-//     }
-// }
-//            });
-//          }
-		  
-// 		   function stopCarousel() {
-//            var owl = $('.twocols');
-//            owl.trigger('destroy.owl.carousel');
-//            owl.addClass('off');
-//          }
 
 
        
